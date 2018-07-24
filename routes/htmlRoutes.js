8dfdb4e1c6bdf33e175aff2ebc3bab9b793ef36a
gugucode <meiyuechang@gmail.com>
2018-07-24 18:40:59 -0500

    remove dbExamples from htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Example.findAll({}).then(function() {
       if (req.isAuthenticated()) {
         res.render("index", {
           msg: "Welcome",
