eb30f28e607e1c73bb80607015c272af42da042d
Amirah Islam <amirahaislam@gmail.com>
2018-07-23 20:00:54 -0500

    added modal

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-      res.render("profile", {
+      res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
       });
